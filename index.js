e13fdaad3b310808760426c422106bd2b33e6a95
Ertholin <ertholinulysse@gmail.com>
2024-02-25 08:29:12 -0500

    no-feat: create 3 arrays to store the 3 parts for the random messages

---
+++ index.js
@@ -0,0 +1,5 @@
+// Motivational Subject
+
+const subjects = ['Quality questions', 'Life', 'Never', 'Nothing great was ever', 'Quality'];
+const verbs = ['is', 'achieved ', 'create', 'is', 'gives'];
+const objects = ['a habit', 'up', 'without enthusiasm', 'perfect', 'a quality life'];
