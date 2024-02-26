60a1b489a83824cbb612b964c7b20733628bca95
Ertholin <ertholinulysse@gmail.com>
2024-02-26 14:57:05 -0500

    no-feat: create the randomly function what are going to choose randomly the element in each array

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 const subjects = ['Quality questions', 'Life', 'Never', 'Nothing great was ever', 'Quality'];
 const verbs = ['is', 'achieved ', 'create', 'is', 'gives'];
 const objects = ['a habit', 'up', 'without enthusiasm', 'perfect', 'a quality life'];
+
+const randomly = (array1, array2, array3) => {
+    let sub = array1[Math.floor(Math.random() * array1.length)];
+    let ver = array2[Math.floor(Math.random() * array2.length)];
+    let obj = array3[Math.floor(Math.random() * array2.length)];
+
+    return `${sub} ${ver} ${obj}`;
+
+}
+
