f0cf711d610c4a41469c131dde3039999efeafc2
Ertholin <ertholinulysse@gmail.com>
2024-02-27 11:03:22 -0500

    no feat: Put ascii art and sentences together, then format

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const randomNumber = num => {
 const collectiveSuccess = {
     feelings : ['bad', 'sad', 'Nothing great was ever', 'down'],
     reactions : ['remember where you come', 'doing great ', 'create', 'try to not give up'],
-    nextSteps : ['will come', 'will make you proud', 'come with enthusiasm', 'perfect', 'gives you a quality life']
+    nextSteps : ['will come', 'will make you proud', 'come with enthusiasm', 'will be perfect', 'gives you a quality life']
 }
 
 let behaviors = [];
@@ -16,38 +16,49 @@ const asciiArt = [
     / @ @ \       
    ( > º < )     
      >>x<<       
-    /  O  \,
-    
+    /  O  \ `,
+
+    `
     (\_/)
    (='.'=)
-   (")_("),
+   (")_(")`,
+   `
     .--.                    
-    '--' ,
-    /\                
-   /__\,
-   (^._.^),
+    '--' `,
+    `
+     /\                
+    /__\
+   (^._.^)`,
+   `
      ____
    <(o.o )>   
     `
 ]
 
-for(let prop in collectiveSuccess){
-    let optIdx = randomNumber(collectiveSuccess[prop].length)
+console.log(asciiArt[randomNumber(asciiArt.length)]);
 
+for(let prop in collectiveSuccess){
+    let optIdx = randomNumber(collectiveSuccess[prop].length);
+    
     switch(prop){
         case 'feelings' : 
             behaviors.push(`If never you feel "${collectiveSuccess[prop][optIdx]}"`)
             break;
         case 'reactions' :
-            behaviors.push(`Just ${collectiveSuccess[prop][optIdx]}`)
+            behaviors.push(`Just "${collectiveSuccess[prop][optIdx]}"`)
             break;
         case 'nextSteps' : 
-            behaviors.push(`And the result ${collectiveSuccess[prop][optIdx]}`)
+            behaviors.push(`And the result "${collectiveSuccess[prop][optIdx]}"`)
             break;
         default : 
             behaviors.push(`Nothing to say`);
     }
 }
 
-console.log(behaviors);
+formatArray = array => {
+    const formatted = behaviors.join('\n');
+    console.log(formatted);
+}
+
+formatArray(behaviors)
 
