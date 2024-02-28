8eefae5d3605124115426b4e4216ac8545e0b6bc
Ertholin <ertholinulysse@gmail.com>
2024-02-27 21:23:27 -0500

    no feat: setting up everything

---
+++ index.js
@@ -0,0 +1,64 @@
+const randomNumber = num => {
+    return Math.floor(Math.random() * num);
+}
+
+const collectiveSuccess = {
+    feelings : ['bad', 'sad', 'Nothing great was ever achieve', 'down'],
+    reactions : ['remember where you come', 'doing great ', 'create', 'never give up'],
+    nextSteps : ['tell youserlf ', 'will make you proud', 'come with enthusiasm', 'will be perfect', 'gives you a quality life']
+}
+
+let behaviors = [];
+
+const asciiArt = [
+    `
+     |\_/|       
+    / @ @ \       
+   ( > º < )     
+     >>x<<       
+    /  O  \ `,
+
+    `
+    (\_/)
+   (='.'=)
+   (")_(")`,
+   `
+    .--.                    
+    '--' `,
+    `
+     /\                
+    /__\
+   (^._.^)`,
+   `
+     ____
+   <(o.o )>   
+    `
+]
+
+console.log(asciiArt[randomNumber(asciiArt.length)]);
+
+
+for(let prop in collectiveSuccess){
+    let optIdx = randomNumber(collectiveSuccess[prop].length);
+    
+    switch(prop){
+        case 'feelings' : 
+            behaviors.push(`If you ever feel like "${collectiveSuccess[prop][optIdx]}"`)
+            break;
+        case 'reactions' :
+            behaviors.push(`Then Just "${collectiveSuccess[prop][optIdx]}"`)
+            break;
+        case 'nextSteps' : 
+            behaviors.push(`Just tell yourself "${collectiveSuccess[prop][optIdx]}"`)
+            break;
+        default : 
+            behaviors.push(`Nothing to say`);
+    }
+}
+
+formatArray = array => {
+    const formatted = behaviors.join('\n');
+    console.log(formatted);
+}
+
+formatArray(behaviors);
